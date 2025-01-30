b5a36f4bdefb6fbcd5e8ceec84cc6436f7ea8565
rlkiran <rlkiran536@gmail.com>
2025-01-30 16:26:32 +0530

    cors added for local testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.item_routes import item_routes
+from fastapi.middleware.cors import CORSMiddleware
 from database import init_db
 
 async def lifespan(app: FastAPI):
@@ -9,5 +10,12 @@ async def lifespan(app: FastAPI):
     # Cleanup logic on shutdown (if needed)
 
 app = FastAPI(lifespan=lifespan)
-
+# Allow all origins (change this to specific origins if needed)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can replace "*" with ["http://127.0.0.1:5500"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(item_routes)
